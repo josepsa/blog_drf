0d81d9c1e70c3c6fbe328cb6f0f08c0d84c7636b
sajan <sajanjose.withu@gmail.com>
2022-06-16 11:19:22 +0300

    changed url

--- blogDRF/urls.py
+++ blogDRF/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+#from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('accounts/login/', views.LoginView.as_view(), name='login'),
 ]
